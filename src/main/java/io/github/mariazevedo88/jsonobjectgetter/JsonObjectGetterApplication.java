e7751feb7c9c4c28049a0488f6a9ff6cb2e15cfc
Mariana Azevedo <mariana@bsi.ufla.br>
2019-02-11 01:19:59 -0200

    Correcting app main class

--- src/main/java/io/github/mariazevedo88/jsonobjectgetter/JsonObjectGetterApplication.java
+++ src/main/java/io/github/mariazevedo88/jsonobjectgetter/JsonObjectGetterApplication.java
@@ -1,6 +1,7 @@
 package io.github.mariazevedo88.jsonobjectgetter;
 
 import org.apache.log4j.Logger;
+import org.json.simple.parser.ParseException;
 
 import io.github.mariazevedo88.jsonobjectgetter.formatter.CustomJSONFormatter;
 
@@ -8,19 +9,14 @@ public class JsonObjectGetterApplication {
 	
 	private static final Logger logger = Logger.getLogger(JsonObjectGetterApplication.class.getName());
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws ParseException{
 		logger.info("Started Json Object Getter Aplication");
 		
 		CustomJSONFormatter formatter = new CustomJSONFormatter();
 		
 		for(String arg : args) {
-			if(formatter.isValidJson(arg)) {
-				logger.info(formatter.getValidJson());
-			}else {
-				logger.info(CustomJSONFormatter.getInvalidJsonToFormat(arg));
-			}
+			formatter.checkValidityAndFormatObject(arg);
 		}
-			
 	}
 
 }
